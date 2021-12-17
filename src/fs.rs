a1c0ff33b3ccad646c3325e07dc33e723aa0cb7d
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-12-17 11:04:15 -0800

    read function now asks for AsRef<Path>

--- src/fs.rs
+++ src/fs.rs
@@ -1,12 +1,14 @@
+use std::path::Path;
+
 use crate::error::FileError;
 
-pub async fn read<S: AsRef<str>>(path: S) -> Result<Vec<u8>, FileError> {
-    macroquad::prelude::load_file(path.as_ref())
+pub async fn read<S: AsRef<Path>>(path: S) -> Result<Vec<u8>, FileError> {
+    macroquad::prelude::load_file(&path.as_ref().as_os_str().to_string_lossy())
         .await
         .map_err(FileError::Engine)
 }
 
-pub async fn read_to_string<S: AsRef<str>>(path: S) -> Result<String, FileError> {
+pub async fn read_to_string<S: AsRef<Path>>(path: S) -> Result<String, FileError> {
     match read(path).await {
         Ok(bytes) => match String::from_utf8(bytes) {
             Ok(string) => Ok(string),
